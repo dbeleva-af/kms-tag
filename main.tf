77f8212d95f1b8d5beaa054c877759692d618042
dbeleva-af <dbeleva_af@abv.bg>
2023-10-06 13:33:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.67.0"
+      version = "5.19.0"
     }
   }
 }
@@ -20,117 +20,31 @@ resource "aws_kms_key" "encryption_key" {
 }
 
 data "aws_iam_policy_document" "encryption_key_policy" {
-  version = "2012-10-17"
   statement {
-    sid     = "AdminCanManageKey"
-    effect  = "Allow"
-    actions = ["kms:*"]
-    principals {
-      identifiers = ["arn:aws:iam::${local.current_account_id}:root"]
-      type        = "AWS"
-    }
-    resources = ["arn:aws:kms:${local.current_region_name}:${local.current_account_id}:key/*"]
-  }
-  statement {
-    sid = "S3CanEncrypt"
-    principals {
-      identifiers = ["s3.amazonaws.com"]
-      type        = "Service"
-    }
     actions = [
-      "kms:Encrypt",
       "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
+      "kms:GenerateDataKey"
     ]
-    resources = ["arn:aws:kms:${local.current_region_name}:${local.current_account_id}:key/*"]
+
+    resources = ["*"]
+
     condition {
-      test     = "ArnEquals"
-      values   = ["arn:aws:s3:::${var.s3_bucket_name}"]
-      variable = "aws:SourceArn"
+      test     = "ForAnyValue:StringEquals"
+      variable = "kms:EncryptionContext:service"
+      values   = ["pi"]
     }
-  }
-statement {
-    sid = "CloudWatchCanEncrypt"
-    principals {
-      identifiers = ["cloudwatch.amazonaws.com"]
-      type        = "Service"
-    }
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
-    ]
-    resources = ["arn:aws:kms:${local.current_region_name}:${local.current_account_id}:key/*"]
-  }
-  statement {
-    sid = "SNSCanEncrypt"
-    principals {
-      identifiers = ["sns.amazonaws.com"]
-      type        = "Service"
-    }
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
-    ]
-    resources = ["arn:aws:kms:${local.current_region_name}:${local.current_account_id}:key/*"]
+
     condition {
-      test = "ForAnyValue:StringEquals"
-      values = [
-        "arn:aws:sns:${local.current_region_name}:${local.current_account_id}:${local.new_objects_topic_name}",
-        "arn:aws:sns:${local.current_region_name}:${local.current_account_id}:${local.integration_alarm_topic_name}"
-      ]
-      variable = "aws:SourceArn"
+      test     = "ForAnyValue:StringEquals"
+      variable = "kms:EncryptionContext:aws:pi:service"
+      values   = ["rds"]
     }
-  }
-  statement {
-    sid = "SQSCanEncrypt"
-    principals {
-      identifiers = ["sqs.amazonaws.com"]
-      type        = "Service"
-    }
-actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
-    ]
-    resources = ["arn:aws:kms:${local.current_region_name}:${local.current_account_id}:key/*"]
+
     condition {
-      test = "ForAnyValue:StringEquals"
-      values = [
-        "arn:aws:sqs:${local.current_region_name}:${local.current_account_id}:${local.new_objects_queue_name}",
-        "arn:aws:sqs:${local.current_region_name}:${local.current_account_id}:${local.new_objects_dlq_name}"
-      ]
-      variable = "aws:SourceArn"
+      test     = "ForAnyValue:StringEquals"
+      variable = "kms:EncryptionContext:aws:rds:db-id"
+      values   = ["db-AAAAABBBBBCCCCCDDDDDEEEEE", "db-EEEEEDDDDDCCCCCBBBBBAAAAA"]
     }
-  }
-  statement {
-    sid = "LogForwardingRoleCanDecrypt"
-    principals {
-      #identifiers = [aws_iam_role.log_forwarding_role.arn]
-      type        = "AWS"
-    }
-    actions   = ["kms:Decrypt"]
-    resources = ["arn:aws:kms:${local.current_region_name}:${local.current_account_id}:key/*"]
-    condition {
-      test     = "StringEquals"
-      values   = [var.trusted_vpc_id]
-      variable = "aws:SourceVpc"
-    }
-    condition {
-      test     = "StringEquals"
-      values   = [var.trusted_account_id]
-      variable = "aws:PrincipalAccount"
-    }
+
   }
 }
-
-
